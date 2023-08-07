38679aefa04b8fe2f8666497fbd9f5d7ba77aa6c
artem <fastfoot1990@gmail.com>
2023-08-07 23:14:39 +0200

    herVjope

---
+++ Git/script.js
@@ -0,0 +1,122 @@
+'use strict'
+
+let number = 4.6;
+
+console.log(4/0);
+console.log('string' * 9);cd
+
+const persone = "Alex";
+
+const bool = false;
+
+console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+name: "John",
+age: 25,
+isMarried: false
+};
+
+console.log(obj.name);
+console.log(obj["name"]);
+
+
+
+// const arr = ['a','b','c'];
+
+// arr[10] = '3456';
+
+// console.log(arr);
+
+// const arr0b = {
+//     a:'a',
+//     1:'b',
+//     2:'c',
+//     abc: {
+//         df: [ {}, {}],
+
+//         def: {
+
+//         }
+//     }
+// };
+
+// const b = 'b';
+
+// // arr0b.b = '1234';
+// arr0b[b] = '1234';
+
+// console.log(arr0b['b']);
+// console.log(arr0b.b);
+
+// // const obj = {a: 1, b:2};
+
+// const obj = {
+//     'Anna': 500,
+//     'Alice': 800
+// };
+
+
+
+// let admin;
+// let name = 'John';
+
+// name = admin;
+// alert(admin);
+
+// let storeName = '7eleven';
+// let storeDescription = {
+//     budget: 10000,
+//     employees:['Josh', 'Tony', 'Lary'],
+//     products: {
+//         apple: 250,
+//         orange: 300
+//     },
+//     open:true
+// };
+
+// alert('hello');
+
+// const result = confirm("are you here?");
+// console.log(result);
+
+// const answer = +prompt("Vam est 18?", "18");
+// console.log(answer + 5);
+
+
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Скока лет тебе?', '');
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "ivan";
+
+// alert(`Prriver, ${user}`);
+
+
+let incr = 10,
+    decr = 10;
+
+    // incr++;
+    // decr--;
+
+    console.log(++incr);
+    console.log(--decr);
+
+    console.log(5%2);
+
+console.log(2 + 2 * 2 !== '6');
+
+const isChecked = false,
+    isClose = false;
+
+    console.log(isChecked || !isClose);
+
